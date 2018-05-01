6d087681189f6bf509c95d72a07b121e2fe9467c
devbaggett <devabaggett@gmail.com>
2018-04-30 21:23:34 -0700

    fixes to file extension

---
+++ swift_fund_1.swift/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+//var type: Float = "Rectangle"
+//var description: String = "A quadrilateral with four right angles"
+//let width: Double = 5
+//let height: Int = 10.5
+//var area: Double = width * height
+//height += 1
+//width += 1
+//area = width * height
+//// Note how you can "interpolate" variables into Strings using the following syntax
+//print("The shape is a \(type) or \(description) with an area of \(area)"
+
+var type: Float = 4.1
+var description: String = "A quadrilateral with four right angles"
+var width: Double = 5.1
+var height: Double = 10
+var area: Double = width * height
+height += 1
+width += 1
+area = width * height
+// Note how you can "interpolate" variables into Strings using the following syntax
+print("The shape is a \(type) or \(description) with an area of \(area)")
+
+
